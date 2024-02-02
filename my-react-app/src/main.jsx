6ddbb8d3e107d38cef7fd89e426f0c176051f7f3
hari0082 <2430680@rts-365.dk>
2024-02-02 11:04:24 +0100

    Add all pages

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Welcome from "./pages/Welcome.jsx"; // Adjusted import
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Search from "./pages/Search.jsx";
+import Schedule from "./pages/Schedule.jsx";
+import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
+import Classview from "./pages/Classview.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route index element={<Welcome />} />
+      <Route path="/søgeside" element={<SearchPage />} />
+      <Route path="/kalender" element={<Calender />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
